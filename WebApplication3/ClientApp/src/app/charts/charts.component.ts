861e99e56e6df00a4684cf5aa1af4e6c19f15249
Jonas Iversen <jonas.iversen@outlook.com>
2019-01-30 09:27:31 +0100

    Byttet navmeny.

--- WebApplication3/ClientApp/src/app/charts/charts.component.ts
+++ WebApplication3/ClientApp/src/app/charts/charts.component.ts
@@ -8,19 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class ChartsComponent implements OnInit {
   showDashboard: boolean;
   showProject: boolean;
+  showReport: boolean;
 
   ngOnInit() {
     this.showDashboard = true;
     this.showProject = false;
+    this.showReport = false;
   }
 
   toProject() {
     this.showProject = true;
     this.showDashboard = false;
+    this.showReport = false;
+  }
+
+  toReport() {
+    this.showReport = true;
+    this.showDashboard = false;
+    this.showProject = false;
   }
 
   backToDashboard() {
     this.showProject = false;
     this.showDashboard = true;
+    this.showReport = false;
   }
 }
